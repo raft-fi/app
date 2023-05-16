75e994c4ad62c381d052c79e288a43f05caa0b68
Lazar <Lazar__93@hotmail.com>
2023-05-16 11:30:14 +0200

    Fix default provider

--- src/hooks/useProvider.ts
+++ src/hooks/useProvider.ts
@@ -1,5 +1,8 @@
 import { JsonRpcProvider } from 'ethers';
 import { of } from 'rxjs';
 
-const DEFAULT_VALUE = new JsonRpcProvider(import.meta.env.VITE_RPC_URL);
+const defaultRpcUrl = import.meta.env.VITE_GOERLI_RPC_URL;
+
+// TODO - Select RPC URL based on current user network (or use default if wallet is not connected)
+const DEFAULT_VALUE = new JsonRpcProvider(defaultRpcUrl);
 export const provider$ = of(DEFAULT_VALUE);
