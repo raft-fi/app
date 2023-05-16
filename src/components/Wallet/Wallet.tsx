8e0e95890356be8a1c8c70ae86ecc25183419d0f
Lazar <Lazar__93@hotmail.com>
2023-05-16 11:23:42 +0200

    fix

--- src/components/Wallet/Wallet.tsx
+++ src/components/Wallet/Wallet.tsx
@@ -21,10 +21,16 @@ init({
   wallets: [injected, ledger, walletConnect],
   chains: [
     {
+      id: '0x1',
+      token: 'ETH',
+      label: 'Ethereum Mainnet',
+      rpcUrl: import.meta.env.VITE_MAINNET_RPC_URL,
+    },
+    {
       id: '0x5',
       token: 'GoerliETH',
       label: 'Goerli test network',
-      rpcUrl: import.meta.env.VITE_RPC_URL,
+      rpcUrl: import.meta.env.VITE_GOERLI_RPC_URL,
     },
   ],
   accountCenter: {
