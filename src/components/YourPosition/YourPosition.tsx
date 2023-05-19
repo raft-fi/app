486571927b33a36c752481ebede1713f0ff3c546
ShawTim <shawtim.liu@gmail.com>
2023-05-20 02:36:44 +0800

    rework for "Your Position"

--- src/components/YourPosition/YourPosition.tsx
+++ src/components/YourPosition/YourPosition.tsx
@@ -1,197 +1,191 @@
-import { Decimal, DecimalFormat } from '@tempusfinance/decimal';
-import { FC, memo, useCallback, useMemo, useState } from 'react';
-import { ButtonWrapper, TokenLogo } from 'tempus-ui';
-import { useProtocolStats, useTokenPrices } from '../../hooks';
-import { getTokenValues } from '../../utils';
-import { COLLATERAL_BASE_TOKEN, DISPLAY_BASE_TOKEN } from '../../constants';
-import { Icon, Typography, ValuesBox, ValueLabel } from '../shared';
+import { R_TOKEN } from '@raft-fi/sdk';
+import { DecimalFormat } from '@tempusfinance/decimal';
+import { FC, memo, useMemo } from 'react';
+import { TokenLogo } from 'tempus-ui';
+import {
+  COLLATERAL_BASE_TOKEN,
+  COLLATERAL_TOKEN_UI_PRECISION,
+  DISPLAY_BASE_TOKEN,
+  R_TOKEN_UI_PRECISION,
+  USD_UI_PRECISION,
+} from '../../constants';
+import { useCollateralBalance, useDebtBalance, useTokenPrices } from '../../hooks';
+import { getCollateralRatioColor, getTokenValues } from '../../utils';
+import { Typography } from '../shared';
 
 import './YourPosition.scss';
-import { R_TOKEN } from '@raft-fi/sdk';
 
 const YourPosition: FC = () => {
-  const [expanded, setExpanded] = useState<boolean>(true);
-
-  const protocolStats = useProtocolStats();
+  const collateralBalance = useCollateralBalance();
+  const debtBalance = useDebtBalance();
   const tokenPriceMap = useTokenPrices();
 
-  const displayBaseTokenValues = useMemo(() => {
-    return getTokenValues(Decimal.ONE, tokenPriceMap[DISPLAY_BASE_TOKEN], DISPLAY_BASE_TOKEN);
-  }, [tokenPriceMap]);
-
-  const collateralTotalSupplyValues = useMemo(() => {
-    if (!protocolStats || !displayBaseTokenValues.price || !tokenPriceMap[COLLATERAL_BASE_TOKEN]) {
+  const collateralTokenValues = useMemo(
+    () => getTokenValues(collateralBalance, tokenPriceMap[COLLATERAL_BASE_TOKEN], COLLATERAL_BASE_TOKEN),
+    [collateralBalance, tokenPriceMap],
+  );
+  const debtTokenValues = useMemo(
+    () => getTokenValues(debtBalance, tokenPriceMap[R_TOKEN], R_TOKEN),
+    [debtBalance, tokenPriceMap],
+  );
+  const collateralInDisplayToken = useMemo(() => {
+    if (
+      !collateralTokenValues.value ||
+      !tokenPriceMap[DISPLAY_BASE_TOKEN] ||
+      tokenPriceMap[DISPLAY_BASE_TOKEN].isZero()
+    ) {
       return null;
     }
 
-    const amount = tokenPriceMap[COLLATERAL_BASE_TOKEN].mul(protocolStats.collateralSupply).div(
-      displayBaseTokenValues.price,
-    );
+    const value = collateralTokenValues.value.div(tokenPriceMap[DISPLAY_BASE_TOKEN]);
 
-    return getTokenValues(amount, tokenPriceMap[DISPLAY_BASE_TOKEN], DISPLAY_BASE_TOKEN);
-  }, [displayBaseTokenValues?.price, protocolStats, tokenPriceMap]);
-
-  const debtTotalSupplyValues = useMemo(() => {
-    if (!protocolStats) {
-      return null;
-    }
-
-    return getTokenValues(protocolStats.debtSupply, tokenPriceMap[R_TOKEN], R_TOKEN);
-  }, [protocolStats, tokenPriceMap]);
+    return getTokenValues(value, tokenPriceMap[DISPLAY_BASE_TOKEN], DISPLAY_BASE_TOKEN);
+  }, [collateralTokenValues.value, tokenPriceMap]);
 
   const collateralizationRatio = useMemo(() => {
-    if (!collateralTotalSupplyValues?.value || !debtTotalSupplyValues?.value) {
+    if (!collateralInDisplayToken?.value || !debtTokenValues.value) {
       return null;
     }
 
-    if (collateralTotalSupplyValues.value.isZero()) {
-      return Decimal.ZERO;
-    }
-
-    return collateralTotalSupplyValues.value.div(debtTotalSupplyValues.value);
-  }, [collateralTotalSupplyValues?.value, debtTotalSupplyValues?.value]);
-
-  const raftTokenPriceFormatted = useMemo(() => {
-    if (!tokenPriceMap[R_TOKEN]) {
+    if (debtTokenValues.value.isZero()) {
       return null;
     }
 
-    return DecimalFormat.format(tokenPriceMap[R_TOKEN].toRounded(2), {
-      style: 'currency',
-      currency: '$',
-      fractionDigits: 2,
-      pad: true,
-    });
-  }, [tokenPriceMap]);
+    return collateralInDisplayToken.value.div(debtTokenValues.value).mul(100);
+  }, [collateralInDisplayToken?.value, debtTokenValues.value]);
 
-  const collateralizationRatioFormatted = useMemo(() => {
-    if (!collateralizationRatio) {
-      return null;
+  const collateralAmountFormatted = useMemo(
+    () =>
+      collateralInDisplayToken?.amount
+        ? DecimalFormat.format(collateralInDisplayToken.amount, {
+            style: 'decimal',
+            fractionDigits: COLLATERAL_TOKEN_UI_PRECISION,
+          })
+        : null,
+    [collateralInDisplayToken?.amount],
+  );
+  const collateralValueFormatted = useMemo(
+    () =>
+      collateralInDisplayToken?.value
+        ? DecimalFormat.format(collateralInDisplayToken.value, {
+            style: 'decimal',
+            fractionDigits: USD_UI_PRECISION,
+          })
+        : null,
+    [collateralInDisplayToken?.value],
+  );
+  const debtAmountFormatted = useMemo(
+    () =>
+      debtTokenValues.amount
+        ? DecimalFormat.format(debtTokenValues.amount, {
+            style: 'decimal',
+            fractionDigits: R_TOKEN_UI_PRECISION,
+          })
+        : null,
+    [debtTokenValues.amount],
+  );
+  const debtValueFormatted = useMemo(
+    () =>
+      debtTokenValues.value
+        ? DecimalFormat.format(debtTokenValues.value, {
+            style: 'decimal',
+            fractionDigits: USD_UI_PRECISION,
+          })
+        : null,
+    [debtTokenValues.value],
+  );
+  const collateralizationRatioFormatted = useMemo(
+    () =>
+      collateralizationRatio
+        ? DecimalFormat.format(collateralizationRatio, {
+            style: 'decimal',
+            fractionDigits: USD_UI_PRECISION,
+          })
+        : null,
+    [collateralizationRatio],
+  );
+  const collateralRatioColor = useMemo(() => getCollateralRatioColor(collateralizationRatio), [collateralizationRatio]);
+  const collateralRatioLabel = useMemo(() => {
+    switch (collateralRatioColor) {
+      case 'text-success':
+        return 'Healthy';
+      case 'text-warning':
+        return 'Unhealthy';
+      case 'text-error':
+        return 'Dangerous';
+      default:
+        return null;
     }
-
-    return DecimalFormat.format(collateralizationRatio, {
-      style: 'percentage',
-      fractionDigits: 2,
-      pad: true,
-    });
-  }, [collateralizationRatio]);
-
-  const onToggleExpanded = useCallback(() => setExpanded(expanded => !expanded), []);
+  }, [collateralRatioColor]);
 
   return (
     <div className="raft__your-position">
-      <div className="raft__your-position__header">
-        <Typography variant="subtitle" weight="medium">
-          Your Position
+      <div className="raft__your-position__collateral">
+        <Typography variant="body-tertiary" weight="semi-bold" color="text-accent">
+          YOUR COLLATERAL
         </Typography>
-        <div className="raft__your-position__actions">
-          <ButtonWrapper onClick={onToggleExpanded}>
-            <Icon variant={expanded ? 'chevron-up' : 'chevron-down'} />
-          </ButtonWrapper>
+        <div className="raft__your-position__collateral__amount">
+          <TokenLogo type={`token-${DISPLAY_BASE_TOKEN}`} size="small" />
+          <div className="raft__your-position__collateral__amount__number">
+            <Typography variant="title" weight="medium">
+              {collateralAmountFormatted ?? '---'}
+            </Typography>
+            <Typography variant="subtitle" weight="medium">
+              {DISPLAY_BASE_TOKEN}
+            </Typography>
+          </div>
+        </div>
+        <div className="raft__your-position__collateral__value__number">
+          <Typography variant="body-tertiary" color="text-secondary">
+            $
+          </Typography>
+          <Typography variant="body-primary" weight="medium" color="text-secondary">
+            {collateralValueFormatted ?? '---'}
+          </Typography>
         </div>
       </div>
-      <div className={`raft__your-position__body ${expanded ? 'raft__your-position-expanded' : ''}`}>
-        <div className="raft__your-position__stat-token">
-          <div className="raft__your-position__stat">
-            <TokenLogo type={`token-${DISPLAY_BASE_TOKEN}`} />
-            <div className="raft__your-position__stat__separator" />
-            <div className="raft__your-position__stat__data">
-              <Typography
-                className="raft__your-position__stat__data__title"
-                variant="body-tertiary"
-                weight="semi-bold"
-                color="text-accent"
-              >
-                Collateral amount
-              </Typography>
-              <div className="raft__your-position__stat__data__value">
-                <ValueLabel value={collateralTotalSupplyValues?.amountFormattedMultiplier || '---'} />
-              </div>
-            </div>
-            <div className="raft__your-position__stat__separator" />
-            <div className="raft__your-position__stat__data">
-              <Typography
-                className="raft__your-position__stat__data__title"
-                variant="body-tertiary"
-                weight="semi-bold"
-                color="text-accent"
-              >
-                Value
-              </Typography>
-              <div className="raft__your-position__stat__data__value">
-                <ValueLabel value={collateralTotalSupplyValues?.valueFormattedMultiplier || '---'} />
-              </div>
-            </div>
-            <div className="raft__your-position__stat__separator" />
-            <div className="raft__your-position__stat__data">
-              <Typography
-                className="raft__your-position__stat__data__title"
-                variant="body-tertiary"
-                weight="semi-bold"
-                color="text-accent"
-              >
-                Price
-              </Typography>
-              <div className="raft__your-position__stat__data__value">
-                <ValueLabel value={displayBaseTokenValues.priceFormattedIntegral ?? '---'} />
-              </div>
-            </div>
-          </div>
-          <div className="raft__your-position__stat">
-            <TokenLogo type={`token-${R_TOKEN}`} />
-            <div className="raft__your-position__stat__separator" />
-            <div className="raft__your-position__stat__data">
-              <Typography
-                className="raft__your-position__stat__data__title"
-                variant="body-tertiary"
-                weight="semi-bold"
-                color="text-accent"
-              >
-                Debt amount
-              </Typography>
-              <div className="raft__your-position__stat__data__value">
-                <ValueLabel value={debtTotalSupplyValues?.amountFormattedMultiplier || '---'} />
-              </div>
-            </div>
-            <div className="raft__your-position__stat__separator" />
-            <div className="raft__your-position__stat__data">
-              <Typography
-                className="raft__your-position__stat__data__title"
-                variant="body-tertiary"
-                weight="semi-bold"
-                color="text-accent"
-              >
-                Value
-              </Typography>
-              <div className="raft__your-position__stat__data__value">
-                <ValueLabel value={debtTotalSupplyValues?.valueFormattedMultiplier || '---'} />
-              </div>
-            </div>
-            <div className="raft__your-position__stat__separator" />
-            <div className="raft__your-position__stat__data">
-              <Typography
-                className="raft__your-position__stat__data__title"
-                variant="body-tertiary"
-                weight="semi-bold"
-                color="text-accent"
-              >
-                Price
-              </Typography>
-              <div className="raft__your-position__stat__data__value">
-                <ValueLabel value={raftTokenPriceFormatted || '---'} />
-              </div>
-            </div>
+      <div className="raft__your-position__debt">
+        <Typography variant="body-tertiary" weight="semi-bold" color="text-accent">
+          YOUR DEBT
+        </Typography>
+        <div className="raft__your-position__debt__amount">
+          <TokenLogo type={`token-${R_TOKEN}`} size="small" />
+          <div className="raft__your-position__debt__amount__number">
+            <Typography variant="title" weight="medium">
+              {debtAmountFormatted ?? '---'}
+            </Typography>
+            <Typography variant="subtitle" weight="medium">
+              {R_TOKEN}
+            </Typography>
           </div>
         </div>
-        <ValuesBox
-          values={[
-            {
-              id: 'collateralizationRatio',
-              label: 'Collateralization ratio',
-              value: collateralizationRatioFormatted || '---',
-            },
-          ]}
-        />
+        <div className="raft__your-position__debt__value__number">
+          <Typography variant="body-tertiary" color="text-secondary">
+            $
+          </Typography>
+          <Typography variant="body-primary" weight="medium" color="text-secondary">
+            {debtValueFormatted ?? '---'}
+          </Typography>
+        </div>
+      </div>
+      <div className="raft__your-position__ratio">
+        <Typography variant="body-tertiary" weight="semi-bold" color="text-accent">
+          COLLATERALIZATION
+        </Typography>
+        <div className="raft__your-position__ratio__percent">
+          <Typography variant="title" weight="medium">
+            {collateralizationRatioFormatted ?? '---'}
+          </Typography>
+          <Typography variant="subtitle" weight="medium">
+            %
+          </Typography>
+        </div>
+        <div className="raft__your-position__ratio__status">
+          <div className={`raft__your-position__ratio__status__color ${collateralRatioColor}`} />
+          <Typography variant="body-primary" weight="medium" color="text-secondary">
+            {collateralRatioLabel ?? '---'}
+          </Typography>
+        </div>
       </div>
     </div>
   );
