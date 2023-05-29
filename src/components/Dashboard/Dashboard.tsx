fd7d8d4d2bfd8e26b4105effc63e6e55c03d620b
ShawTim <shawtim.liu@gmail.com>
2023-05-29 17:06:10 +0800

    loading skeleton (#398)

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import { memo, useMemo } from 'react';
-import { useCollateralBalance, useDebtBalance, useNetwork } from '../../hooks';
+import { useCollateralBalance, useDebtBalance, useNetwork, useAppLoaded } from '../../hooks';
+import LoadingDashbaord from '../LoadingDashboard';
 import ProtocolStats from '../ProtocolStats';
 import YourPosition from '../YourPosition';
 import OpenPosition from '../OpenPosition';
@@ -9,6 +10,7 @@ import TransactionModal from '../TransactionModal';
 import './Dashboard.scss';
 
 const Dashboard = () => {
+  const appLoaded = useAppLoaded();
   const collateralBalance = useCollateralBalance();
   const debtBalance = useDebtBalance();
   const { isWrongNetwork } = useNetwork();
@@ -18,6 +20,10 @@ const Dashboard = () => {
   }, [collateralBalance, debtBalance]);
 
   const shouldShowAdjustPosition = userHasBorrowed && collateralBalance && debtBalance && !isWrongNetwork;
+
+  if (!appLoaded) {
+    return <LoadingDashbaord />;
+  }
 
   return (
     <div className="raft__dashboard">
---
+++ src/components/LoadingDashboard/LoadingPosition.tsx
@@ -0,0 +1,45 @@
+import { memo } from 'react';
+
+const LoadingPosition = () => (
+  <svg
+    className="raft__loading-position"
+    width="856"
+    height="449"
+    viewBox="0 0 856 449"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="856" height="449" rx="8" fill="url(#paint0_linear_474_3894)" />
+    <rect opacity="0.15" x="32" y="32" width="100" height="14" rx="7" fill="#003652" />
+    <rect opacity="0.1" x="32" y="78" width="80" height="8" rx="4" fill="#003652" />
+    <g opacity="0.07">
+      <rect x="32" y="102" width="380" height="48" rx="8" fill="#003652" />
+    </g>
+    <rect opacity="0.1" x="444" y="78" width="80" height="8" rx="4" fill="#003652" />
+    <g opacity="0.07">
+      <rect x="444" y="102" width="380" height="48" rx="8" fill="#003652" />
+    </g>
+    <rect opacity="0.1" x="32" y="182" width="80" height="8" rx="4" fill="#003652" />
+    <circle opacity="0.15" cx="42" cy="222.5" r="10" fill="#003652" />
+    <rect opacity="0.15" x="62" y="215.5" width="100" height="14" rx="7" fill="#003652" />
+    <circle opacity="0.15" cx="42" cy="271.5" r="10" fill="#003652" />
+    <rect opacity="0.15" x="62" y="264.5" width="100" height="14" rx="7" fill="#003652" />
+    <circle opacity="0.15" cx="42" cy="320.5" r="10" fill="#003652" />
+    <rect opacity="0.15" x="62" y="313.5" width="100" height="14" rx="7" fill="#003652" />
+    <rect opacity="0.1" x="444" y="182" width="80" height="8" rx="4" fill="#003652" />
+    <rect opacity="0.15" x="444" y="215.5" width="100" height="14" rx="7" fill="#003652" />
+    <rect opacity="0.1" x="444" y="271" width="80" height="8" rx="4" fill="#003652" />
+    <rect opacity="0.15" x="444" y="304.5" width="100" height="14" rx="7" fill="#003652" />
+    <g opacity="0.15">
+      <rect x="32" y="369" width="792" height="48" rx="8" fill="#003652" />
+    </g>
+    <defs>
+      <linearGradient id="paint0_linear_474_3894" x1="428" y1="0" x2="428" y2="449" gradientUnits="userSpaceOnUse">
+        <stop stopColor="#C0CAD2" stopOpacity="0.12" />
+        <stop offset="1" stopColor="#9FB1C1" stopOpacity="0.17" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+
+export default memo(LoadingPosition);
