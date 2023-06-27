98aa9fb52a5a7a175c38ed7fb407f9edd6aa5272
Daniele D <danieledamianiuk@gmail.com>
2023-06-27 15:39:56 +0100

    Add `totalRSupply` to `useProtocolStats` (#620)

--- src/hooks/useProtocolStats.ts
+++ src/hooks/useProtocolStats.ts
@@ -36,8 +36,9 @@ const fetchData = (provider: JsonRpcProvider) => {
       from(stats.fetchCollateralSupply()),
       from(stats.fetchDebtSupply()),
       from(stats.fetchOpenPositionCount()),
+      from(stats.getTotalRSupply()),
     ]).pipe(
-      map(() => {
+      map(([, , , totalRSupply]) => {
         if (!stats.collateralSupply || !stats.debtSupply || !stats.openPositionCount) {
           return null;
         }
@@ -46,6 +47,7 @@ const fetchData = (provider: JsonRpcProvider) => {
           collateralSupply: stats.collateralSupply,
           debtSupply: stats.debtSupply,
           openPositions: stats.openPositionCount,
+          totalRSupply: totalRSupply,
         };
       }),
       catchError(error => {
