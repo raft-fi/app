7e4e547866b9d4da38ffb3dda37a678f46887b02
ShawTim <shawtim.liu@gmail.com>
2023-07-03 16:24:45 +0800

    TVL should show the amount of display token, not underlying token

--- src/components/ProtocolStats/CollateralStatsBreakdown.tsx
+++ src/components/ProtocolStats/CollateralStatsBreakdown.tsx
@@ -6,7 +6,7 @@ import {
   SUPPORTED_UNDERLYING_TOKENS,
   USD_UI_PRECISION,
 } from '../../constants';
-import { useProtocolStats, useTokenPrices } from '../../hooks';
+import { useCollateralConversionRates, useProtocolStats, useTokenPrices } from '../../hooks';
 import { SupportedUnderlyingCollateralToken, TokenDecimalMap, TokenGenericMap } from '../../interfaces';
 import { formatDecimal } from '../../utils';
 import { Tooltip, Typography } from '../shared';
@@ -14,31 +14,61 @@ import { Tooltip, Typography } from '../shared';
 const CollateralStatsBreakdown = () => {
   const protocolStats = useProtocolStats();
   const tokenPriceMap = useTokenPrices();
+  const collateralConversionRateMap = useCollateralConversionRates();
 
   const collateralSupplyMap = useMemo(
     () => protocolStats?.collateralSupply ?? ({} as TokenDecimalMap<SupportedUnderlyingCollateralToken>),
     [protocolStats?.collateralSupply],
   );
-  const collateralSupplyAmountFormattedMap = SUPPORTED_UNDERLYING_TOKENS.reduce(
-    (map, underlyingToken) => ({
-      ...map,
-      [underlyingToken]: formatDecimal(collateralSupplyMap[underlyingToken], COLLATERAL_TOKEN_UI_PRECISION),
-    }),
-    {} as TokenGenericMap<SupportedUnderlyingCollateralToken, string>,
+  const collateralSupplyAmountInDisplayTokenMap = useMemo(
+    () =>
+      SUPPORTED_UNDERLYING_TOKENS.reduce((map, underlyingToken) => {
+        const displayToken = SUPPORTED_COLLATERAL_TOKEN_SETTINGS[underlyingToken].displayBaseToken;
+        const rate = collateralConversionRateMap[displayToken];
+        const supply = collateralSupplyMap[underlyingToken];
+
+        if (!rate || !supply) {
+          return map;
+        }
+
+        return {
+          ...map,
+          [underlyingToken]: supply.mul(rate),
+        };
+      }, {} as TokenGenericMap<SupportedUnderlyingCollateralToken, string>),
+    [collateralConversionRateMap, collateralSupplyMap],
   );
-  const collateralSupplyValueFormattedMap = SUPPORTED_UNDERLYING_TOKENS.reduce((map, underlyingToken) => {
-    const collateralSupply = collateralSupplyMap[underlyingToken];
-    const tokenPrice = tokenPriceMap[underlyingToken];
+  const collateralSupplyAmountInDisplayTokenFormattedMap = useMemo(
+    () =>
+      SUPPORTED_UNDERLYING_TOKENS.reduce(
+        (map, underlyingToken) => ({
+          ...map,
+          [underlyingToken]: formatDecimal(
+            collateralSupplyAmountInDisplayTokenMap[underlyingToken],
+            COLLATERAL_TOKEN_UI_PRECISION,
+          ),
+        }),
+        {} as TokenGenericMap<SupportedUnderlyingCollateralToken, string>,
+      ),
+    [collateralSupplyAmountInDisplayTokenMap],
+  );
+  const collateralSupplyValueFormattedMap = useMemo(
+    () =>
+      SUPPORTED_UNDERLYING_TOKENS.reduce((map, underlyingToken) => {
+        const collateralSupply = collateralSupplyMap[underlyingToken];
+        const tokenPrice = tokenPriceMap[underlyingToken];
 
-    if (!collateralSupply || !tokenPrice) {
-      return map;
-    }
+        if (!collateralSupply || !tokenPrice) {
+          return map;
+        }
 
-    return {
-      ...map,
-      [underlyingToken]: formatDecimal(collateralSupply.mul(tokenPrice), USD_UI_PRECISION),
-    };
-  }, {} as TokenGenericMap<SupportedUnderlyingCollateralToken, string>);
+        return {
+          ...map,
+          [underlyingToken]: formatDecimal(collateralSupply.mul(tokenPrice), USD_UI_PRECISION),
+        };
+      }, {} as TokenGenericMap<SupportedUnderlyingCollateralToken, string>),
+    [collateralSupplyMap, tokenPriceMap],
+  );
 
   return (
     <Tooltip className="raft__protocol-stats__collateral-breakdown">
@@ -55,7 +85,7 @@ const CollateralStatsBreakdown = () => {
             <div className="raft__protocol-stats__collateral-breakdown__token-values">
               <div className="raft__protocol-stats__collateral-breakdown__token-amount">
                 <Typography variant="body" weight="medium">
-                  {collateralSupplyAmountFormattedMap[underlyingToken] ?? '---'}&nbsp;
+                  {collateralSupplyAmountInDisplayTokenFormattedMap[underlyingToken] ?? '---'}&nbsp;
                 </Typography>
                 <Typography variant="body2">
                   {SUPPORTED_COLLATERAL_TOKEN_SETTINGS[underlyingToken].displayBaseToken}
