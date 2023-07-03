93b94a83c5292f4dbd37414a02aa7faefff3e1ba
ShawTim <shawtim.liu@gmail.com>
2023-07-03 19:43:50 +0800

    dummy code for useLeverage hook

--- src/hooks/useLeverage.ts
+++ src/hooks/useLeverage.ts
@@ -1,5 +1,5 @@
 import { v4 as uuid } from 'uuid';
-import { ERC20PermitSignatureStruct, R_TOKEN, UserPosition, ManagePositionStep } from '@raft-fi/sdk';
+import { ERC20PermitSignatureStruct, ManagePositionStep, R_TOKEN, UserPosition } from '@raft-fi/sdk';
 import { bind } from '@react-rxjs/core';
 import { createSignal } from '@react-rxjs/utils';
 import { Decimal } from '@tempusfinance/decimal';
@@ -34,6 +34,28 @@ import { wallet$ } from './useWallet';
 import { walletSigner$ } from './useWalletSigner';
 import { getNullTokenMap, isSignatureValid } from '../utils';
 
+// TODO: dummy func for now
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
+async function* getLeverageSteps(_collateralChange: Decimal, _leverage: Decimal, _options: any) {
+  yield {
+    type: {
+      name: 'approve',
+    },
+    stepNumber: 1,
+    numberOfSteps: 2,
+    action: async () => ({} as TransactionResponse),
+  } as LeveragePositionStep;
+
+  return {
+    type: {
+      name: 'manage',
+    },
+    stepNumber: 2,
+    numberOfSteps: 2,
+    action: async () => ({} as TransactionResponse),
+  } as LeveragePositionStep;
+}
+
 const DEFAULT_VALUE = {
   pending: false,
   statusType: null,
@@ -47,70 +69,77 @@ const DEFAULT_STEPS = {
 };
 const GAS_LIMIT_MULTIPLIER = new Decimal(2);
 
+// TODO: provided by SDK
+type LeveragePositionStep = ManagePositionStep;
 type UserPositionMap = TokenGenericMap<
   SupportedUnderlyingCollateralToken,
   Nullable<UserPosition<SupportedUnderlyingCollateralToken>>
 >;
 type SignatureMap = TokenGenericMap<SupportedToken, Nullable<ERC20PermitSignatureStruct>>;
-type ManagePositionStepsGenerator = AsyncGenerator<ManagePositionStep, void, ERC20PermitSignatureStruct | undefined>;
-type ManagePositionFunc = () => void;
-type RequestManagePositionStepFunc = (request: ManagePositionStepsRequest) => void;
-type ManagePositionStatusType = 'whitelist' | 'approve' | 'permit' | 'manage';
+type LeveragePositionStepsGenerator = AsyncGenerator<
+  LeveragePositionStep,
+  void,
+  ERC20PermitSignatureStruct | undefined
+>;
+type LeveragePositionFunc = () => void;
+type RequestLeveragePositionStepFunc = (request: LeveragePositionStepsRequest) => void;
+type LeveragePositionStatusType = 'whitelist' | 'approve' | 'permit' | 'leverage';
 
 const userPositionMap: UserPositionMap =
   getNullTokenMap<SupportedUnderlyingCollateralToken>(SUPPORTED_UNDERLYING_TOKENS);
 const signatureMap: SignatureMap = getNullTokenMap<SupportedToken>(SUPPORTED_TOKENS);
 
-interface ManagePositionStepsRequest {
+interface LeveragePositionStepsRequest {
   underlyingCollateralToken: SupportedUnderlyingCollateralToken;
   collateralToken: SupportedCollateralToken;
   collateralChange: Decimal;
-  debtChange: Decimal;
+  leverage: Decimal;
   isClosePosition?: boolean;
 }
 
-interface ManagePositionStatus {
+interface LeveragePositionStatus {
   pending: boolean;
   success?: boolean;
-  statusType: Nullable<ManagePositionStatusType>;
-  request: Nullable<ManagePositionStepsRequest>;
+  statusType: Nullable<LeveragePositionStatusType>;
+  request: Nullable<LeveragePositionStepsRequest>;
   response?: TransactionResponse;
   signature?: ERC20PermitSignatureStruct;
   txHash?: string;
   error?: Error;
 }
 
-interface ManagePositionStepsStatus {
+interface LeveragePositionStepsStatus {
   pending: boolean;
-  request: Nullable<ManagePositionStepsRequest>;
-  result: Nullable<ManagePositionStep>;
-  generator: Nullable<ManagePositionStepsGenerator>;
+  request: Nullable<LeveragePositionStepsRequest>;
+  result: Nullable<LeveragePositionStep>;
+  generator: Nullable<LeveragePositionStepsGenerator>;
   error?: Error;
 }
 
-interface ManagePositionStepsResponse {
-  request: ManagePositionStepsRequest;
-  result: Nullable<ManagePositionStep>;
-  generator: Nullable<ManagePositionStepsGenerator>;
+interface LeveragePositionStepsResponse {
+  request: LeveragePositionStepsRequest;
+  result: Nullable<LeveragePositionStep>;
+  generator: Nullable<LeveragePositionStepsGenerator>;
 }
 
-const [managePositionStepsRequest$, setManagePositionStepsRequest] = createSignal<ManagePositionStepsRequest>();
-const managePositionStepsStatus$ = new BehaviorSubject<ManagePositionStepsStatus>(DEFAULT_STEPS);
-const managePositionStatus$ = new BehaviorSubject<ManagePositionStatus>(DEFAULT_VALUE);
+const [leveragePositionStepsRequest$, setLeveragePositionStepsRequest] = createSignal<LeveragePositionStepsRequest>();
+const leveragePositionStepsStatus$ = new BehaviorSubject<LeveragePositionStepsStatus>(DEFAULT_STEPS);
+const leveragePositionStatus$ = new BehaviorSubject<LeveragePositionStatus>(DEFAULT_VALUE);
 
-const managePosition$ = managePositionStepsStatus$.pipe(
+const leveragePosition$ = leveragePositionStepsStatus$.pipe(
   withLatestFrom(wallet$),
-  map<[ManagePositionStepsStatus, Nullable<BrowserProvider>], Nullable<ManagePositionFunc>>(
+  map<[LeveragePositionStepsStatus, Nullable<BrowserProvider>], Nullable<LeveragePositionFunc>>(
     ([status, walletProvider]) => {
       const { pending, request, result, generator, error } = status;
 
       if (!pending && !error && result && generator && walletProvider) {
         return async () => {
           const notificationId = uuid();
-          const statusType = result.type.name;
+          // TODO: when SDK provide steps type we dont need this
+          const statusType = result.type.name === 'manage' ? 'leverage' : result.type.name;
 
           try {
-            managePositionStatus$.next({ pending: true, request, statusType });
+            leveragePositionStatus$.next({ pending: true, request, statusType });
 
             if (statusType === 'approve' || statusType === 'permit') {
               notification$.next({
@@ -142,7 +171,7 @@ const managePosition$ = managePositionStepsStatus$.pipe(
                 throw receiptFetchFailed;
               }
 
-              managePositionStatus$.next({
+              leveragePositionStatus$.next({
                 pending: false,
                 statusType,
                 success: true,
@@ -157,12 +186,12 @@ const managePosition$ = managePositionStepsStatus$.pipe(
                   notificationType: 'approval-success',
                   timestamp: Date.now(),
                 });
-              } else if (statusType === 'manage') {
+              } else if (statusType === 'leverage') {
                 // reset signature map after txn success
                 SUPPORTED_TOKENS.forEach(token => (signatureMap[token] = null));
               }
             } else {
-              managePositionStatus$.next({ pending: false, request, statusType, success: true, signature });
+              leveragePositionStatus$.next({ pending: false, request, statusType, success: true, signature });
 
               if (result.type.token && ([...SUPPORTED_TOKENS] as string[]).includes(result.type.token)) {
                 signatureMap[result.type.token] = signature;
@@ -177,7 +206,7 @@ const managePosition$ = managePositionStepsStatus$.pipe(
               }
             }
 
-            managePositionStepsStatus$.next({
+            leveragePositionStepsStatus$.next({
               request,
               result: null,
               generator,
@@ -185,7 +214,7 @@ const managePosition$ = managePositionStepsStatus$.pipe(
             });
 
             const nextStep = await generator.next(!isTransactionResponse ? signature : undefined);
-            managePositionStepsStatus$.next({
+            leveragePositionStepsStatus$.next({
               request,
               result: nextStep.value ?? null,
               generator,
@@ -198,8 +227,8 @@ const managePosition$ = managePositionStepsStatus$.pipe(
               txnHash: txnResponse.hash,
             });
           } catch (error) {
-            console.error(`useManage (error) - Failed to execute ${statusType}!`, error);
-            managePositionStatus$.next({ pending: false, request, statusType, error: error as Error });
+            console.error(`useLeverage (error) - Failed to execute ${statusType}!`, error);
+            leveragePositionStatus$.next({ pending: false, request, statusType, error: error as Error });
 
             if (statusType === 'approve' || statusType === 'permit') {
               notification$.next({
@@ -217,8 +246,8 @@ const managePosition$ = managePositionStepsStatus$.pipe(
   ),
 );
 
-const requestManagePositionStep$ = walletSigner$.pipe(
-  map<Nullable<JsonRpcSigner>, RequestManagePositionStepFunc>(signer => (request: ManagePositionStepsRequest) => {
+const requestLeveragePositionStep$ = walletSigner$.pipe(
+  map<Nullable<JsonRpcSigner>, RequestLeveragePositionStepFunc>(signer => (request: LeveragePositionStepsRequest) => {
     if (!signer) {
       return;
     }
@@ -230,11 +259,11 @@ const requestManagePositionStep$ = walletSigner$.pipe(
       userPositionMap[request.underlyingCollateralToken] = userPosition;
     }
 
-    setManagePositionStepsRequest(request);
+    setLeveragePositionStepsRequest(request);
   }),
 );
 
-const tokenMapsLoaded$ = combineLatest([managePositionStepsRequest$, tokenWhitelists$, tokenAllowances$]).pipe(
+const tokenMapsLoaded$ = combineLatest([leveragePositionStepsRequest$, tokenWhitelists$, tokenAllowances$]).pipe(
   map(([request, tokenWhitelistMap, tokenAllowanceMap]) => {
     const { collateralToken } = request;
     const isDelegateWhitelisted = tokenWhitelistMap[collateralToken];
@@ -245,13 +274,13 @@ const tokenMapsLoaded$ = combineLatest([managePositionStepsRequest$, tokenWhitel
   }),
   distinctUntilChanged(),
 );
-const distinctRequest$ = managePositionStepsRequest$.pipe(
+const distinctRequest$ = leveragePositionStepsRequest$.pipe(
   distinctUntilChanged(
     (prev, current) =>
       prev.collateralToken === current.collateralToken &&
       prev.underlyingCollateralToken === current.underlyingCollateralToken &&
       prev.collateralChange.equals(current.collateralChange) &&
-      prev.debtChange.equals(current.debtChange) &&
+      prev.leverage.equals(current.leverage) &&
       prev.isClosePosition === current.isClosePosition,
   ),
 );
@@ -259,42 +288,29 @@ const stream$ = combineLatest([distinctRequest$, tokenMapsLoaded$]).pipe(
   filter(([, tokenMapsLoaded]) => tokenMapsLoaded), // only to process steps when all maps are loaded
   withLatestFrom(tokenWhitelists$, tokenAllowances$),
   concatMap(([[request], tokenWhitelistMap, tokenAllowanceMap]) => {
-    const { underlyingCollateralToken, collateralToken, collateralChange, debtChange, isClosePosition } = request;
+    const { underlyingCollateralToken, collateralToken, collateralChange, leverage, isClosePosition } = request;
 
     const isDelegateWhitelisted = tokenWhitelistMap[collateralToken] ?? undefined;
     const collateralTokenAllowance = tokenAllowanceMap[collateralToken] ?? undefined;
-    const rTokenAllowance = tokenAllowanceMap[R_TOKEN] ?? undefined;
     const collateralPermitSignature = isSignatureValid(signatureMap[collateralToken])
       ? (signatureMap[collateralToken] as ERC20PermitSignatureStruct)
       : undefined;
-    const rPermitSignature = isSignatureValid(signatureMap[R_TOKEN])
-      ? (signatureMap[R_TOKEN] as ERC20PermitSignatureStruct)
-      : undefined;
-
-    if (collateralChange.isZero() && debtChange.isZero()) {
-      return of({
-        request,
-        result: null,
-        generator: null,
-      } as ManagePositionStepsResponse);
-    }
 
     try {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const userPosition = userPositionMap[
         underlyingCollateralToken
       ] as UserPosition<SupportedUnderlyingCollateralToken>;
       const actualCollateralChange = isClosePosition ? Decimal.ZERO : collateralChange;
-      const actualDebtChange = isClosePosition ? Decimal.MAX_DECIMAL.mul(-1) : debtChange;
 
-      managePositionStepsStatus$.next({ pending: true, request, result: null, generator: null });
+      leveragePositionStepsStatus$.next({ pending: true, request, result: null, generator: null });
 
-      const steps = userPosition.getManageSteps(actualCollateralChange, actualDebtChange, {
+      // TODO: dummy code for now
+      const steps = getLeverageSteps(actualCollateralChange, leverage, {
         collateralToken,
         isDelegateWhitelisted,
         collateralTokenAllowance,
-        rTokenAllowance,
         collateralPermitSignature,
-        rPermitSignature,
         gasLimitMultiplier: GAS_LIMIT_MULTIPLIER,
       });
       const nextStep$ = from(steps.next());
@@ -306,18 +322,18 @@ const stream$ = combineLatest([distinctRequest$, tokenMapsLoaded$]).pipe(
               request,
               result: nextStep.value,
               generator: steps,
-            } as ManagePositionStepsResponse;
+            } as unknown as LeveragePositionStepsResponse;
           }
 
           return {
             request,
             result: null,
             generator: steps,
-          } as ManagePositionStepsResponse;
+          } as unknown as LeveragePositionStepsResponse;
         }),
         catchError(error => {
           console.error(
-            `useManagePositionSteps (catchError) - failed to get manage position steps for ${underlyingCollateralToken}!`,
+            `useLeveragePositionSteps (catchError) - failed to get leverage position steps for ${underlyingCollateralToken}!`,
             error,
           );
           return of({
@@ -325,12 +341,12 @@ const stream$ = combineLatest([distinctRequest$, tokenMapsLoaded$]).pipe(
             result: null,
             generator: null,
             error,
-          } as ManagePositionStepsResponse);
+          } as LeveragePositionStepsResponse);
         }),
       );
     } catch (error) {
       console.error(
-        `useManagePositionSteps (catch) - failed to get manage position steps for ${underlyingCollateralToken}!`,
+        `useLeveragePositionSteps (catch) - failed to get leverage position steps for ${underlyingCollateralToken}!`,
         error,
       );
       return of({
@@ -338,38 +354,41 @@ const stream$ = combineLatest([distinctRequest$, tokenMapsLoaded$]).pipe(
         result: null,
         generator: null,
         error,
-      } as ManagePositionStepsResponse);
+      } as LeveragePositionStepsResponse);
     }
   }),
-  tap<ManagePositionStepsResponse>(response => {
-    managePositionStepsStatus$.next({ ...response, pending: false });
+  tap<LeveragePositionStepsResponse>(response => {
+    leveragePositionStepsStatus$.next({ ...response, pending: false });
   }),
 );
 
-const [managePosition] = bind<Nullable<ManagePositionFunc>>(managePosition$, null);
-const [requestManagePositionStep] = bind<Nullable<RequestManagePositionStepFunc>>(requestManagePositionStep$, null);
-const [managePositionStatus] = bind<ManagePositionStatus>(managePositionStatus$, DEFAULT_VALUE);
-const [managePositionStepsStatus] = bind<ManagePositionStepsStatus>(managePositionStepsStatus$, DEFAULT_STEPS);
+const [leveragePosition] = bind<Nullable<LeveragePositionFunc>>(leveragePosition$, null);
+const [requestLeveragePositionStep] = bind<Nullable<RequestLeveragePositionStepFunc>>(
+  requestLeveragePositionStep$,
+  null,
+);
+const [leveragePositionStatus] = bind<LeveragePositionStatus>(leveragePositionStatus$, DEFAULT_VALUE);
+const [leveragePositionStepsStatus] = bind<LeveragePositionStepsStatus>(leveragePositionStepsStatus$, DEFAULT_STEPS);
 
-export const useManage = (): {
-  managePositionStatus: ManagePositionStatus;
-  managePositionStepsStatus: ManagePositionStepsStatus;
-  managePosition: Nullable<ManagePositionFunc>;
-  requestManagePositionStep: Nullable<RequestManagePositionStepFunc>;
+export const useLeverage = (): {
+  leveragePositionStatus: LeveragePositionStatus;
+  leveragePositionStepsStatus: LeveragePositionStepsStatus;
+  leveragePosition: Nullable<LeveragePositionFunc>;
+  requestLeveragePositionStep: Nullable<RequestLeveragePositionStepFunc>;
 } => ({
-  managePositionStatus: managePositionStatus(),
-  managePositionStepsStatus: managePositionStepsStatus(),
-  managePosition: managePosition(),
-  requestManagePositionStep: requestManagePositionStep(),
+  leveragePositionStatus: leveragePositionStatus(),
+  leveragePositionStepsStatus: leveragePositionStepsStatus(),
+  leveragePosition: leveragePosition(),
+  requestLeveragePositionStep: requestLeveragePositionStep(),
 });
 
 let subscriptions: Subscription[];
 
-export const subscribeManageStatus = (): void => {
-  unsubscribeManageStatus();
+export const subscribeLeverageStatus = (): void => {
+  unsubscribeLeverageStatus();
   subscriptions = [stream$.subscribe()];
 };
-export const unsubscribeManageStatus = (): void => subscriptions?.forEach(subscription => subscription.unsubscribe());
-export const resetManageStatus = (): void => {
-  managePositionStatus$.next(DEFAULT_VALUE);
+export const unsubscribeLeverageStatus = (): void => subscriptions?.forEach(subscription => subscription.unsubscribe());
+export const resetLeverageStatus = (): void => {
+  leveragePositionStatus$.next(DEFAULT_VALUE);
 };
