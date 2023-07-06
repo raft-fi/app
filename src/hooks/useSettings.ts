d4884224e2cb5b7d23e1140824da3d120653b824
ShawTim <shawtim.liu@gmail.com>
2023-07-07 01:53:43 +0800

    settings popup and setting perference (#655)

---
+++ src/hooks/useSettings.ts
@@ -0,0 +1,49 @@
+import { useCallback } from 'react';
+import { distinctUntilChanged } from 'rxjs';
+import { state, useStateObservable } from '@react-rxjs/core';
+import { createSignal } from '@react-rxjs/utils';
+import { Decimal } from '@tempusfinance/decimal';
+import { DEFAULT_SLIPPAGE } from '../constants';
+
+// TODO: SDK to export this
+export type SwapRouter = '1inch';
+
+export interface SettingOptions {
+  router: SwapRouter;
+  slippage: Decimal;
+}
+
+const [rawRouter$, setRouter] = createSignal<SwapRouter>();
+const [rawSlippage$, setSlippage] = createSignal<Decimal>();
+
+const router$ = rawRouter$.pipe(distinctUntilChanged());
+const slippage$ = rawSlippage$.pipe(distinctUntilChanged());
+
+const stateRouter$ = state(router$, '1inch');
+const stateSlippage$ = state(slippage$, new Decimal(DEFAULT_SLIPPAGE));
+
+export function useSettingOptions(): [SettingOptions, (partial: Partial<SettingOptions>) => void] {
+  const router = useStateObservable(stateRouter$);
+  const slippage = useStateObservable(stateSlippage$);
+
+  const setPartialOption = useCallback((partial: Partial<SettingOptions>) => {
+    Object.keys(partial).forEach(field => {
+      switch (field) {
+        case 'router':
+          if (partial.router !== undefined) {
+            setRouter(partial.router);
+          }
+          break;
+        case 'slippage':
+          if (partial.slippage !== undefined) {
+            setSlippage(partial.slippage);
+          }
+          break;
+        default:
+          break;
+      }
+    });
+  }, []);
+
+  return [{ router, slippage }, setPartialOption];
+}
