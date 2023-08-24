ee4d37dd1125877e78713bdb42a9dcbd8f341e92
Lazar <Lazar__93@hotmail.com>
2023-08-24 20:18:38 +0200

    Handle withdraw action button state and input errors

--- src/components/Savings/Savings.tsx
+++ src/components/Savings/Savings.tsx
@@ -5,6 +5,7 @@ import { R_TOKEN } from '@raft-fi/sdk';
 import { ButtonWrapper } from 'tempus-ui';
 import {
   useAppLoaded,
+  useCurrentUserSavings,
   useManageSavings,
   useNetwork,
   useSavingsMaxDeposit,
@@ -27,6 +28,7 @@ const Savings = () => {
   const wallet = useWallet();
   const tokenBalanceMap = useTokenBalances();
   const savingsMaxDeposit = useSavingsMaxDeposit();
+  const currentUserSavings = useCurrentUserSavings();
   const { manageSavingsStatus, manageSavings, manageSavingsStepsStatus, requestManageSavingsStep } = useManageSavings();
 
   const [isAddCollateral, setIsAddCollateral] = useState<boolean>(true);
@@ -99,7 +101,7 @@ const Savings = () => {
 
   const hasNonEmptyInput = useMemo(() => !amountParsed.isZero(), [amountParsed]);
 
-  const hasEnoughRTokenBalance = useMemo(() => {
+  const hasEnoughRToDeposit = useMemo(() => {
     // In case R token balance is still loading
     if (!rTokenBalance) {
       return false;
@@ -107,6 +109,15 @@ const Savings = () => {
 
     return amountParsed.lte(rTokenBalance);
   }, [amountParsed, rTokenBalance]);
+
+  const hasEnoughRToWithdraw = useMemo(() => {
+    // In case current user savings are still loading
+    if (!currentUserSavings) {
+      return false;
+    }
+
+    return amountParsed.lte(currentUserSavings);
+  }, [amountParsed, currentUserSavings]);
 
   const isPositionWithinDepositCap = useMemo(() => {
     // In case savingsMaxDeposit is still loading
@@ -127,11 +138,15 @@ const Savings = () => {
       return 'Unsupported network';
     }
 
-    if (!hasEnoughRTokenBalance) {
+    if (isAddCollateral && !hasEnoughRToDeposit) {
       return 'Insufficient funds';
     }
 
-    if (!isPositionWithinDepositCap) {
+    if (!isAddCollateral && !hasEnoughRToWithdraw) {
+      return 'Insufficient balance';
+    }
+
+    if (isAddCollateral && !isPositionWithinDepositCap) {
       return 'Deposit capacity reached, please try again later';
     }
 
@@ -160,7 +175,9 @@ const Savings = () => {
   }, [
     walletConnected,
     isWrongNetwork,
-    hasEnoughRTokenBalance,
+    hasEnoughRToDeposit,
+    hasEnoughRToWithdraw,
+    isAddCollateral,
     isPositionWithinDepositCap,
     executionSteps,
     executionType,
@@ -180,14 +197,13 @@ const Savings = () => {
   const hasInputFilled = useMemo(() => !amountParsed.isZero(), [amountParsed]);
 
   const canExecuteDeposit = useMemo(
-    () => Boolean(hasInputFilled && hasEnoughRTokenBalance && !isWrongNetwork && isPositionWithinDepositCap),
-    [hasEnoughRTokenBalance, hasInputFilled, isPositionWithinDepositCap, isWrongNetwork],
+    () => Boolean(hasInputFilled && hasEnoughRToDeposit && !isWrongNetwork && isPositionWithinDepositCap),
+    [hasEnoughRToDeposit, hasInputFilled, isPositionWithinDepositCap, isWrongNetwork],
   );
 
   const canExecuteWithdraw = useMemo(() => {
-    // TODO - Handle withdraw disabled state
-    return true;
-  }, []);
+    return Boolean(hasInputFilled && hasEnoughRToWithdraw && !isWrongNetwork);
+  }, [hasEnoughRToWithdraw, hasInputFilled, isWrongNetwork]);
 
   const canExecute = useMemo(() => {
     if (isAddCollateral) {
@@ -196,25 +212,24 @@ const Savings = () => {
     return canExecuteWithdraw;
   }, [canExecuteDeposit, canExecuteWithdraw, isAddCollateral]);
 
-  const errorMessageWithdraw = useMemo(() => {
-    // TODO - Handle withdraw error messages
-    return '';
-  }, []);
-
-  const errorMessageDeposit = useMemo(() => {
+  const errorMessage = useMemo(() => {
     if (!walletConnected) {
       return;
     }
 
-    if (!hasEnoughRTokenBalance) {
+    if (isAddCollateral && !hasEnoughRToDeposit) {
       return 'Insufficient funds';
+    }
+
+    if (!isAddCollateral && !hasEnoughRToWithdraw) {
+      return 'Insufficient balance';
     }
 
     if (isWrongNetwork) {
       return 'You are connected to unsupported network. Please switch to Ethereum Mainnet.';
     }
 
-    if (!isPositionWithinDepositCap) {
+    if (isAddCollateral && !isPositionWithinDepositCap) {
       const maxDeposit = formatCurrency(savingsMaxDeposit, {
         currency: R_TOKEN,
         fractionDigits: 2,
@@ -222,14 +237,15 @@ const Savings = () => {
 
       return `Deposit amount exceeds max deposit amount of ${maxDeposit}. Please reduce the deposit amount.`;
     }
-  }, [hasEnoughRTokenBalance, isPositionWithinDepositCap, isWrongNetwork, savingsMaxDeposit, walletConnected]);
-
-  const errorMessage = useMemo(() => {
-    if (isAddCollateral) {
-      return errorMessageDeposit;
-    }
-    return errorMessageWithdraw;
-  }, [errorMessageDeposit, errorMessageWithdraw, isAddCollateral]);
+  }, [
+    hasEnoughRToDeposit,
+    hasEnoughRToWithdraw,
+    isAddCollateral,
+    isPositionWithinDepositCap,
+    isWrongNetwork,
+    savingsMaxDeposit,
+    walletConnected,
+  ]);
 
   const onAction = useCallback(() => {
     manageSavings?.();
