9c2e4c57da7ef441d74c6c777659a1a7b42a2439
Daniele Damiani <danieledamianiuk@gmail.com>
2023-08-24 15:07:26 +0100

    Update `Stats` box to include `Your savings`

--- src/components/Savings/Stats/Stats.tsx
+++ src/components/Savings/Stats/Stats.tsx
@@ -1,5 +1,5 @@
 import { TokenLogo } from 'tempus-ui';
-import { Typography } from '../../shared';
+import { Icon, Tooltip, TooltipWrapper, Typography } from '../../shared';
 
 import './Stats.scss';
 
@@ -8,8 +8,44 @@ const Stats = () => {
   const TVL = '1,234,556.00';
   const capacity = '1,234,556.00';
 
+  const showSavings = true;
+  const savings = '1,234,556.00';
+
   return (
     <div className="raft__savings__stats">
+      {showSavings && (
+        <div className="raft__savings__stats__item raft__savings__stats__item-inverse">
+          <div className="raft__savings__stats__item-title">
+            <Typography variant="overline" weight="semi-bold" color="text-accent-inverted">
+              YOUR SAVINGS
+            </Typography>
+            <TooltipWrapper
+              tooltipContent={
+                <Tooltip className="raft__leveragePositionAfter__infoTooltip">
+                  <Typography variant="body2">
+                    Your savings include your deposited amount plus earned rewards.
+                  </Typography>
+                </Tooltip>
+              }
+              placement="bottom"
+            >
+              <Icon variant="info" size="tiny" color="var(--textAccentInverted)" />
+            </TooltipWrapper>
+          </div>
+          <div className="raft__savings__stats__item-token">
+            <TokenLogo type="token-R" size="small" />
+            <div className="raft__savings__stats__item-value">
+              <Typography variant="heading1" color="text-primary-inverted">
+                {savings}
+              </Typography>
+              <Typography variant="heading2" color="text-primary-inverted">
+                R
+              </Typography>
+            </div>
+          </div>
+        </div>
+      )}
+
       <div className="raft__savings__stats__item">
         <Typography variant="overline" weight="semi-bold" color="text-accent">
           CURRENT YIELD
