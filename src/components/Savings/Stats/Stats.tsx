fa6c44db5c9b32ac6d2d36120471a279bf471e5e
Daniele Damiani <danieledamianiuk@gmail.com>
2023-08-25 12:01:53 +0100

    Update `Savings` page

--- src/components/Savings/Stats/Stats.tsx
+++ src/components/Savings/Stats/Stats.tsx
@@ -1,4 +1,4 @@
-import { FC, useMemo } from 'react';
+import { CSSProperties, FC, useMemo } from 'react';
 import { R_TOKEN } from '@raft-fi/sdk';
 import { TokenLogo } from 'tempus-ui';
 import { DecimalFormat, Decimal } from '@tempusfinance/decimal';
@@ -71,76 +71,92 @@ const Stats: FC<StatsProps> = ({ currentSavings, currentYield, tvl, savingsMaxDe
     });
   }, [savingsMaxDeposit]);
 
+  const leftCapacityProgressBar = useMemo(() => {
+    const capacity = tvl?.div(savingsMaxDeposit || 0).mul(100);
+    return `${capacity}%`;
+  }, [tvl, savingsMaxDeposit]);
+
+  const progressBarStyle: CSSProperties = {
+    width: leftCapacityProgressBar,
+  };
+
   return (
-    <div className="raft__savings__stats">
+    <>
       {currentSavingsFormatted && (
-        <div className="raft__savings__stats__item raft__savings__stats__item-inverse">
-          <div className="raft__savings__stats__item-title">
-            <Typography variant="overline" weight="semi-bold" color="text-accent-inverted">
-              YOUR SAVINGS
-            </Typography>
-            <TooltipWrapper
-              tooltipContent={
-                <Tooltip className="raft__leveragePositionAfter__infoTooltip">
-                  <Typography variant="body2">
-                    Your savings include your deposited amount plus earned rewards.
-                  </Typography>
-                </Tooltip>
-              }
-              placement="bottom"
-            >
-              <Icon variant="info" size="tiny" color="var(--textAccentInverted)" />
-            </TooltipWrapper>
-          </div>
-          <div className="raft__savings__stats__item-token">
-            <TokenLogo type="token-R" size="small" />
-            <ValueLabel
-              value={currentSavingsFormatted}
-              valueSize="heading1"
-              tickerSize="heading2"
-              color="text-primary-inverted"
-            />
+        <div className="raft__savings__stats">
+          <div className="raft__savings__stats__item raft__savings__stats__item-inverse">
+            <div className="raft__savings__stats__item-title">
+              <Typography variant="overline" weight="semi-bold" color="text-accent-inverted">
+                YOUR SAVINGS
+              </Typography>
+              <TooltipWrapper
+                tooltipContent={
+                  <Tooltip className="raft__leveragePositionAfter__infoTooltip">
+                    <Typography variant="body2">
+                      Your savings include your deposited amount plus earned rewards.
+                    </Typography>
+                  </Tooltip>
+                }
+                placement="bottom"
+              >
+                <Icon variant="info" size="tiny" color="var(--textAccentInverted)" />
+              </TooltipWrapper>
+            </div>
+            <div className="raft__savings__stats__item-token">
+              <TokenLogo type="token-R" size="small" />
+              <ValueLabel
+                value={currentSavingsFormatted}
+                valueSize="heading1"
+                tickerSize="heading2"
+                color="text-primary-inverted"
+              />
+            </div>
           </div>
         </div>
       )}
 
-      <div className="raft__savings__stats__item">
-        <Typography variant="overline" weight="semi-bold" color="text-accent">
-          CURRENT YIELD
-        </Typography>
-        {currentYieldFormatted ? (
-          <ValueLabel value={currentYieldFormatted} valueSize="heading1" tickerSize="heading2" label="APR fixed" />
-        ) : (
-          '---'
-        )}
-      </div>
-      <div className="raft__savings__stats__item">
-        <Typography variant="overline" weight="semi-bold" color="text-accent">
-          TOTAL VALUE LOCKED
-        </Typography>
-        <div className="raft__savings__stats__item-token">
-          <TokenLogo type="token-R" size="small" />
-          {currentTvlFormatted ? (
-            <ValueLabel value={currentTvlFormatted} valueSize="heading1" tickerSize="heading2" />
+      <div className="raft__savings__stats">
+        <div className="raft__savings__stats__item">
+          <Typography variant="overline" weight="semi-bold" color="text-accent">
+            CURRENT YIELD
+          </Typography>
+          {currentYieldFormatted ? (
+            <ValueLabel value={currentYieldFormatted} valueSize="heading1" tickerSize="heading2" label="APR fixed" />
           ) : (
             '---'
           )}
         </div>
-      </div>
-      <div className="raft__savings__stats__item">
-        <Typography variant="overline" weight="semi-bold" color="text-accent">
-          REMAINING CAPACITY
-        </Typography>
-        <div className="raft__savings__stats__item-token">
-          <TokenLogo type="token-R" size="small" />
-          {savingsMaxDepositFormatted ? (
-            <ValueLabel value={savingsMaxDepositFormatted} valueSize="heading1" tickerSize="heading2" />
-          ) : (
-            '---'
-          )}
+        <div className="raft__savings__stats__item">
+          <Typography variant="overline" weight="semi-bold" color="text-accent">
+            TOTAL VALUE LOCKED
+          </Typography>
+          <div className="raft__savings__stats__item-token">
+            <TokenLogo type="token-R" size="small" />
+            {currentTvlFormatted ? (
+              <ValueLabel value={currentTvlFormatted} valueSize="heading1" tickerSize="heading2" />
+            ) : (
+              '---'
+            )}
+          </div>
+        </div>
+        <div className="raft__savings__stats__item">
+          <Typography variant="overline" weight="semi-bold" color="text-accent">
+            REMAINING CAPACITY
+          </Typography>
+          <div className="raft__savings__stats__item-token">
+            <TokenLogo type="token-R" size="small" />
+            {savingsMaxDepositFormatted ? (
+              <ValueLabel value={savingsMaxDepositFormatted} valueSize="heading1" tickerSize="heading2" />
+            ) : (
+              '---'
+            )}
+          </div>
+          <div className="raft__savings__stats__capacity__progress-bar__container">
+            <div className="raft__savings__stats__capacity__progress-bar" style={progressBarStyle} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Stats;
