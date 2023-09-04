387e62900be2089721ee16c6b8dac32a9a1f5f56
Lazar Mitic <lazar@tempus.finance>
2023-09-04 08:22:55 +0200

    Remove savings cap info from UI (#803)

--- src/components/Savings/Stats/Stats.tsx
+++ src/components/Savings/Stats/Stats.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, FC, useMemo } from 'react';
+import { FC, useMemo } from 'react';
 import { R_TOKEN } from '@raft-fi/sdk';
 import { TokenLogo } from 'tempus-ui';
 import { DecimalFormat, Decimal } from '@tempusfinance/decimal';
@@ -12,10 +12,9 @@ type StatsProps = {
   currentSavings: Nullable<Decimal>;
   currentYield: Nullable<Decimal>;
   tvl: Nullable<Decimal>;
-  savingsMaxDeposit?: Nullable<Decimal>;
 };
 
-const Stats: FC<StatsProps> = ({ currentSavings, currentYield, tvl, savingsMaxDeposit }) => {
+const Stats: FC<StatsProps> = ({ currentSavings, currentYield, tvl }) => {
   const currentSavingsFormatted = useMemo(() => {
     if (!currentSavings) {
       return null;
@@ -43,20 +42,6 @@ const Stats: FC<StatsProps> = ({ currentSavings, currentYield, tvl, savingsMaxDe
     });
   }, [currentYield]);
 
-  const currentTvlFormatted = useMemo(() => {
-    if (!tvl) {
-      return null;
-    }
-
-    return DecimalFormat.format(tvl, {
-      style: 'currency',
-      currency: R_TOKEN,
-      fractionDigits: R_TOKEN_UI_PRECISION,
-      lessThanFormat: true,
-      pad: true,
-    });
-  }, [tvl]);
-
   const currentTvlMultiplier = useMemo(() => {
     if (!tvl) {
       return null;
@@ -75,51 +60,6 @@ const Stats: FC<StatsProps> = ({ currentSavings, currentYield, tvl, savingsMaxDe
     () => currentSavings && currentSavings.gt(Decimal.ZERO) && currentSavingsFormatted,
     [currentSavings, currentSavingsFormatted],
   );
-
-  const savingsMaxDepositFormatted = useMemo(() => {
-    if (!savingsMaxDeposit) {
-      return null;
-    }
-
-    return DecimalFormat.format(savingsMaxDeposit, {
-      style: 'currency',
-      currency: R_TOKEN,
-      fractionDigits: R_TOKEN_UI_PRECISION,
-      lessThanFormat: true,
-      pad: true,
-    });
-  }, [savingsMaxDeposit]);
-
-  const totalCapacity = useMemo(() => {
-    if (!savingsMaxDeposit || !tvl) {
-      return null;
-    }
-
-    return tvl.add(savingsMaxDeposit);
-  }, [tvl, savingsMaxDeposit]);
-
-  const totalCapacityMultiplier = useMemo(() => {
-    if (!totalCapacity) {
-      return null;
-    }
-
-    return DecimalFormat.format(totalCapacity, {
-      style: 'multiplier',
-      currency: R_TOKEN,
-      fractionDigits: R_TOKEN_UI_PRECISION,
-      lessThanFormat: true,
-      pad: true,
-    });
-  }, [totalCapacity]);
-
-  const leftCapacityProgressBar = useMemo(() => {
-    const capacity = tvl?.div(totalCapacity || -1).mul(100);
-    return `${capacity}%`;
-  }, [tvl, totalCapacity]);
-
-  const progressBarStyle: CSSProperties = {
-    width: leftCapacityProgressBar,
-  };
 
   return (
     <>
@@ -168,63 +108,17 @@ const Stats: FC<StatsProps> = ({ currentSavings, currentYield, tvl, savingsMaxDe
           )}
         </div>
         <div className="raft__savings__stats__item">
-          <TooltipWrapper
-            tooltipContent={
-              <Tooltip className="raft__savings__tvlTooltip-container">
-                <div className="raft__savings__tvlTooltip">
-                  <Typography variant="overline" color="text-secondary">
-                    TOTAL VALUE LOCKED
-                  </Typography>
-                  {currentTvlFormatted ? (
-                    <div className="raft__savings__stats__item-token">
-                      <TokenLogo type="token-R" size="small" />
-                      <ValueLabel value={currentTvlFormatted} valueSize="body" tickerSize="caption" />
-                    </div>
-                  ) : (
-                    '---'
-                  )}
-                  <div className="raft__savings__tvlTooltip-container__spacer" />
-                  <Typography variant="overline" color="text-secondary">
-                    SAVINGS CAPACITY
-                  </Typography>
-                  {savingsMaxDepositFormatted ? (
-                    <div className="raft__savings__stats__item-token">
-                      <TokenLogo type="token-R" size="small" />
-                      <ValueLabel value={savingsMaxDepositFormatted} valueSize="body" tickerSize="caption" />
-                    </div>
-                  ) : (
-                    '---'
-                  )}
-                </div>
-              </Tooltip>
-            }
-            placement="bottom"
-          >
-            <Typography variant="overline" weight="semi-bold" color="text-accent">
-              TOTAL VALUE LOCKED
-            </Typography>
-            <div className="raft__savings__stats__item-token">
-              <TokenLogo type="token-R" size="small" />
-              {currentTvlMultiplier ? (
-                <ValueLabel value={currentTvlMultiplier} valueSize="heading1" tickerSize="heading2" />
-              ) : (
-                '---'
-              )}
-              <div className="raft__savings__divider">
-                <Typography variant="heading1" weight="medium" color="text-disabled">
-                  /
-                </Typography>
-              </div>
-              {totalCapacityMultiplier ? (
-                <ValueLabel value={totalCapacityMultiplier} valueSize="heading1" tickerSize="heading2" />
-              ) : (
-                '---'
-              )}
-            </div>
-            <div className="raft__savings__stats__capacity__progress-bar__container">
-              <div className="raft__savings__stats__capacity__progress-bar" style={progressBarStyle} />
-            </div>
-          </TooltipWrapper>
+          <Typography variant="overline" weight="semi-bold" color="text-accent">
+            TOTAL VALUE LOCKED
+          </Typography>
+          <div className="raft__savings__stats__item-token">
+            <TokenLogo type="token-R" size="small" />
+            {currentTvlMultiplier ? (
+              <ValueLabel value={currentTvlMultiplier} valueSize="heading1" tickerSize="heading2" />
+            ) : (
+              '---'
+            )}
+          </div>
         </div>
       </div>
     </>
